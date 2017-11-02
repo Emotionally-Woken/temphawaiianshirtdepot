3c990be1779567bfba260bf049e431507042fb98
woskxn <iamchangxx@gmail.com>
2017-11-02 12:09:48 -0400

    (feat) update homepage

--- client/components/ReviewList.js
+++ client/components/ReviewList.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
-
+import {List, ListItem} from 'material-ui/List';
+import Subheader from 'material-ui/Subheader';
+import Divider from 'material-ui/Divider';
 
 const ReviewList = (props)=>{
   let {reviews, productId} = props
@@ -12,16 +14,23 @@ const ReviewList = (props)=>{
 
   return (
     <div>
-     { selectedReviews && selectedReviews.map(review => (
-       <div>{review.reviewContent}</div>
-     ))}
+      <List>
+        <Subheader>Reviews</Subheader>
+          { selectedReviews && selectedReviews.map(review => (
+              <ListItem
+                primaryText= {review.user.email.slice(0,3)}
+                secondaryText={review.reviewContent}
+              />
+          ))}
+     </List>
     </div>
   )
 }
 
 const MapState = (state, ownProps) => ({
   reviews: state.reviews.allReviews,
-  productId: ownProps.productId
+  productId: ownProps.productId,
+  users: state.user
 })
 
 export default connect(MapState)(ReviewList)
