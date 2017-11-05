b8a165d738fbd12e95b2423137ead34334158622
apdjz <14apdjz@gmail.com>
2017-11-05 01:50:21 -0500

    creating checkoutform

--- client/components/Checkout.js
+++ client/components/Checkout.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { changeQuantityThunk } from '../store/cart';//fetchItems
 import Cart from './Cart'
 //----------------------------
 // user signup-component 
@@ -16,16 +15,12 @@ class CheckoutForm extends Component {
       title: "wonderful title"
     };
     this.handleChange = this.handleChange.bind(this);
+    this.couponSubmit = this.couponSubmit.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.renderUserAddress = this.renderUserAddress.bind(this)
     this.renderLoginOrSignUp = this.renderLoginOrSignUp.bind(this)
 
   }
-
-  // componentWillMount() {
-  //    // this.props.fetchItems();
-  // }
-
   handleChange(event) {
     const value = event.target.value;
     this.setState({
@@ -33,10 +28,24 @@ class CheckoutForm extends Component {
     });
   }
 
+  couponSubmit(event) {
+    event.preventDefault()
+    const guest = !this.props.isLoggedIn
+    let checkList = {
+      email: (guest ? event.target.email.value : this.props.currentUser.email),
+      address: event.target.address.value,
+      city: event.target.city.value,
+      state: event.target.state.value,
+      zipCode: event.target.zipcode.value,
+      userId: (!guest ? this.props.currentUser.id : null),
+      status: 'CREATED',
+      total: this.props.cart.total
+    }
+  }
+
   handleSubmit(event) {
     event.preventDefault();
     this.props.history.push(`/order/${id}`);
-
   }
 
   renderUserAddress() {
@@ -56,13 +65,33 @@ class CheckoutForm extends Component {
         <button>No mahalo, guest check out</button>
       </div>)
   }
+
   render() {
-    const { cart, user, history } = this.props
+    const { currentUser, isLoggedIn } = this.props
+    return (
+      <div className='container'>
+        <div className='row'>
+          <div className='col'>
+            <div>
+              <span>Checkout {
+                isLoggedIn ? currentUser.username : 'Guest'}</span>
+            </div>
+            <Cart history={history} />
+          </div>
+        </div>
+        {this.checkoutForm()}
+      </div>
+    )
+  }
+
+  checkoutForm() {
+    const { cart, user } = this.props
     console.log(this.props)
     return (
       <div>
         {(user.id && this.renderUserAddress()) || this.renderLoginOrSignUp()}
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={this.couponSubmit}>
+
           <div className="form-group">
             <label htmlFor="checkoutInputCoupon">Coupon</label>
             <input
@@ -77,21 +106,50 @@ class CheckoutForm extends Component {
             Enter a coupon
           </button>
         </form>
+
+        <form onSubmit={this.handleSubmit}>
+          {
+            !this.props.isLoggedIn && (<label>
+              Email:
+            <input type="text" name="email" required />
+            </label>)
+          }
+          <label>
+            Street Address:
+          <input type="text" name="address" required />
+          </label>
+          <label>
+            City:
+          <input type="text" name="city" required />
+          </label>
+          <label>
+            State:
+          <input type="text" name="state" required />
+          </label>
+          <label>
+            zipCode:
+          <input type="text" name="zipcode" required />
+          </label>
+          <input type="submit" value="Submit" required />
+        </form>
         <h1>
           Mahalo, {user.firstName || 'Surfer Dude!'}
         </h1>
-        <Cart history={history} />
         <button disabled={!cart.length} onClick={() => {/*stripe */ }} >Pay!</button>
       </div>
     );
   }
-}
-const mapStateToProps = (state) => ({
-  user: state.user,
-  cart: state.cart
-  //   products: state.products
-});
 
+}
+const mapStateToProps = (state, ownProps) => {
+  return {
+    user: state.user,
+    cart: state.cart,
+    isLoggedIn: !!currentUser.id
+    //   products: state.products
+  }
+
+}
 const mapDispatchToProps = (dispatch) => ({
   // fetchItems: () => dispatch(fetchItems())
 });
--- server/api/email.js
+++ server/api/email.js
@@ -1,33 +1,41 @@
 const router = require('express').Router()
 var nodemailer = require('nodemailer');
 
-module.exports = router
-router.post('/', handleSayHello); // handle the route at yourdomain.com/sayHello
 
-function handleSayHello(req, res) {
-    // Not the movie transporter!
-    var transporter = nodemailer.createTransport({
-        service: 'Gmail',
-        auth: {
-            user: 'azfullstack@gmail.com', // Your email id
-            pass: 'a05439yz' // Your password
-        }
-    });
-    var text = 'Hello world from \n\n' + req.body.name;
+module.exports = router
+router.post('/', orderConfirmation); // handle the route at yourdomain.com/sayHello
+
+var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    //  secure: ,
+    //    port: ,
+    auth: {
+        user: 'azfullstack@gmail.com', // Your real email
+        pass: 'a05439yz' // Your real password
+    },
+    tls: {
+        rejectUnauthorized: false
+    }
+});
+
+function orderConfirmation(req, res, next) {
+    const order = req.body
+    let orderDetail = ``
+    let message = `Congratulations\n Order ${order.id} has been created.\n`
+    message += `Mahalo--Thanks for buying!`
+
     var mailOptions = {
-        from: 'azfullstack@gmail.com>', // sender address
-        to: 'azfullstack@gmail.com', // list of receivers
-        subject: 'Email Example', // Subject line
-        text: text //, // plaintext body
+        from: 'azfullstack@gmail.com', // sender address
+        to: req.body.email, // list of receivers 'azfullstack@gmail.com'
+        subject: `Order ${req.body.id}`, // Subject line
+        text: message //, // plaintext body
         // html: '<b>Hello world ✔</b>' // You can choose to send an HTML body instead
     };
-    transporter.sendMail(mailOptions, function(error, info){
-        if(error){
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
             console.log(error);
-            res.json({yo: 'error'});
-        }else{
+        } else {
             console.log('Message sent: ' + info.response);
-            res.json({yo: info.response});
         }
     });
 }
