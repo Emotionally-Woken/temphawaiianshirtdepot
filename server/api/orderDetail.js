6e26ab254195c2d55f049b7e87ee91c1c1d0fc58
Brian Macdonald <brian.lmacdonald@gmail.com>
2017-11-05 00:51:59 -0400

    stupid file added.

---
+++ server/api/orderDetail.js
@@ -0,0 +1,52 @@
+const router = require('express').Router()
+const { Order, OrderDetail } = require('../db/models')
+module.exports = router
+
+router.post('/:orderId/new', (req, res, next) => {
+  req.body.orderId = req.params.orderId
+  OrderDetail.create(req.body)
+    .then(createdOrderDetail => {
+      res.json(createdOrderDetail)
+    })
+    .catch(next)
+})
+
+router.post('/bulkNew', (req, res, next) => {
+  Promise.all(req.body.orderDetails.map(orderDetail => {
+    orderDetail.orderId = req.body.id
+    OrderDetail.create(orderDetail)
+      .then()
+      .catch(next)
+  }))
+    .then(res.json.bind(res))
+    .catch(next)
+})
+
+router.put('/update/:orderId/:productId', (req, res, next) => {
+  OrderDetail.find({
+    where: {
+      orderId: req.params.orderId,
+      $and: { productId: req.params.productId }
+    }
+  })
+    .then(orderDetail => {
+      orderDetail.update({ quantity: req.body.quantity })
+        .then(updatedOrderDetail => {
+          res.sendStatus(201).end()
+        })
+    })
+    .catch(next)
+})
+
+router.delete(`/remove/:orderId/:productId`, (req, res, next) => {
+  OrderDetail.destroy({
+    where: {
+      orderId: req.params.orderId,
+      $and: { productId: req.params.productId }
+    }
+  })
+    .then(destroyed => {
+      res.sendStatus(200).end()
+    })
+    .catch(next)
+})
