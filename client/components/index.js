1d40d3d266ce43d14dbd1010e857486178b83177
apdjz <14apdjz@gmail.com>
2017-11-07 11:31:26 -0500

    redux-devtools-extension regular depen.

--- client/components/index.js
+++ client/components/index.js
@@ -3,7 +3,7 @@
  * This way, we can import all of our components from the same place, rather than
  * having to figure out which file they belong to!
  */
-export {default as Main} from './Main'
+export {default as Main} from './main'
 export {default as UserHome} from './UserHome'
 export {default as AllProducts} from './AllProducts'
 export {default as Collections} from './Collections'
